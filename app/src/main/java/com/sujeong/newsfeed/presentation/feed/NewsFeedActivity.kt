d1a97f44494f014976a1751c1ca219ead6ad6603
임수정 <tnwjd8116@gmail.com>
2024-10-09 23:16:49 +0900

    뉴스 피드 목록 UI

---
+++ app/src/main/java/com/sujeong/newsfeed/presentation/feed/NewsFeedActivity.kt
@@ -0,0 +1,16 @@
+package com.sujeong.newsfeed.presentation.feed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sujeong.newsfeed.databinding.ActivityNewsFeedBinding
+
+class NewsFeedActivity: AppCompatActivity() {
+    private val binding by lazy {
+        ActivityNewsFeedBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
